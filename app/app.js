67ecb883657d5b3c95a2d934f89536b4df7c5a11
Amir Mehdi Savand <amir@savandbros.com>
2017-07-29 13:30:20 +0400

    Scroll up on each view

--- app/app.js
+++ app/app.js
@@ -14,7 +14,12 @@ app.run(function ($rootScope) {
     date: new Date(),
     shop: "https://shop.almaahadeng.com"
   };
-});
+  /**
+   * @desc Scroll up on each page view
+   */
+  $rootScope.$on('$viewContentLoaded', function () {
+    $anchorScroll();
+  });
 
 app.config(function ($locationProvider) {
   $locationProvider.hashPrefix("");
