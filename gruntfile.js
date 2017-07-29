2e48a92d553265d6b91110e92f8e1bf52c6ca208
Amir Mehdi Savand <amir@savandbros.com>
2017-07-29 13:34:24 +0400

    Fix app name

--- gruntfile.js
+++ gruntfile.js
@@ -272,7 +272,7 @@ module.exports = function (grunt) {
     ngtemplates: {
       dist: {
         options: {
-          module: 'itemBox',
+          module: 'maahadEng',
           htmlmin: '<%= htmlmin.dist.options %>',
           usemin: 'scripts/scripts.js'
         },
