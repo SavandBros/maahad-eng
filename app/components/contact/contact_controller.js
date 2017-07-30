5eb5c6e676503260f0dcec8c47a6e3e904887c6d
Amir Mehdi Savand <amir@savandbros.com>
2017-07-30 06:36:47 +0400

    Make API call for for contact us

--- app/components/contact/contact_controller.js
+++ app/components/contact/contact_controller.js
@@ -1,8 +1,17 @@
 "use strict";
 
-app.controller("ContactController", function ($scope) {
+app.controller("ContactController", function (API, $scope) {
 
   $scope.send = function (form) {
     form.loading = true;
+    var payload = {
+      name: form.name,
+      number: form.number,
+      email: form.email,
+      message: form.message
+    };
+    API.post("contact", payload, {}, function () {
+      form.loading = false;
+    });
   };
 });
