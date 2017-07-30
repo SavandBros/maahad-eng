5cf0d7fbf1d2edbbb0584b1fc0264e62f5386a43
Amir Mehdi Savand <amir@savandbros.com>
2017-07-30 05:14:13 +0400

    Get settings from API in main controller

--- app/components/main/main_controller.js
+++ app/components/main/main_controller.js
@@ -1,10 +1,17 @@
 "use strict";
 
-app.controller("MainController", function ($scope, $state) {
+app.controller("MainController", function (API, $scope, $rootScope, $state) {
 
   function constructor() {
 
     $scope.state = $state;
+
+    /**
+     * @desc Get setting variables
+     */
+    API.get("settings", {}, {}, function (data) {
+      $rootScope.settings = data.data;
+    });
   }
 
   constructor();
