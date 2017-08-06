23d155cf41b52db0994b4462be18b0b1f68dec62
Amir Mehdi Savand <amir@savandbros.com>
2017-08-06 16:52:20 +0400

    Update form handling

--- app/components/contact/contact_controller.js
+++ app/components/contact/contact_controller.js
@@ -4,14 +4,20 @@ app.controller("ContactController", function (API, $scope) {
 
   $scope.send = function (form) {
     form.loading = true;
+
     var payload = {
-      name: form.name,
-      number: form.number,
+      name: form.name || "",
+      number: form.number || "",
       email: form.email,
       message: form.message
     };
-    API.post("contact", payload, {}, function () {
+
+    API.post("messages", payload, {}, function () {
       form.loading = false;
+      form.success = true;
+    }, function () {
+      form.loading = false;
+      form.error = true;
     });
   };
 });
