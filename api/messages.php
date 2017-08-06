fb25b9a49f23f5545a9cd3565a90362acf9e07ac
Amir Mehdi Savand <amir@savandbros.com>
2017-08-06 16:52:01 +0400

    Ability to message

--- api/messages.php
+++ api/messages.php
@@ -6,6 +6,18 @@ if ($method === "GET") {
 
     response($db->select("SELECT * FROM messages LIMIT 100"));
 
-} else if ($method === "PUT") {
+} else if ($method === "POST") {
+
+    if (require_params(["email", "message"], $_POST)) {
+
+        $name = $db->quote($_POST["name"]);
+        $number = $db->quote($_POST["number"]);
+        $email = $db->quote($_POST["email"]);
+        $message = $db->quote($_POST["message"]);
+
+        $db->query("INSERT INTO messages (name, number, email, message) VALUES ($name, $number, $email, $message)");
+
+        $code = $db->affected() ? 201 : 400;
+        response($db->affected(), $code);
     }
 }
