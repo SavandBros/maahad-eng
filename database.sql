f7ee228529f3a5f78cee0463e3788dc2a5df4b67
Amir Mehdi Savand <amir@savandbros.com>
2017-08-06 15:54:29 +0400

    Add `messages` table

--- database.sql
+++ database.sql
@@ -1,9 +1,19 @@
 -- Database structure setup.
 -- Note: A user with a database named 'maahad-eng' should be created first.
 -- Version: 2017-07-30
+
 CREATE TABLE `settings` (
  `setting` varchar(50) NOT NULL,
  `label` varchar(50) NOT NULL,
  `value` varchar(100) NOT NULL,
  PRIMARY KEY (`setting`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
+CREATE TABLE `messages` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `name` varchar(50) NOT NULL,
+ `number` varchar(50) NOT NULL,
+ `email` varchar(200) NOT NULL,
+ `message` text NOT NULL,
+ PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
