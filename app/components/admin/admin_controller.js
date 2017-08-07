80e9c33d90e975f24ae9061136aaa76c000ed48a
Amir Mehdi Savand <amir@savandbros.com>
2017-08-07 17:49:28 +0400

    Add section handling functions

--- app/components/admin/admin_controller.js
+++ app/components/admin/admin_controller.js
@@ -3,15 +3,47 @@
 app.controller("AdminController", function ($scope, API, localStorageService) {
 
   function constructor() {
+    /**
+     * @desc Sections of admin page
+     */
+    $scope.sections = ["messages", "information", "authenticate"];
 
+    /**
+     * @desc Current selected section
+     */
+    $scope.activateSection("messages");
   }
+
+  $scope.activateSection = function (section) {
+    if (!$scope.getAuthentication()) {
+      $scope.currentSection = "authenticate";
+      return;
+    }
+
+    $scope.currentSection = section;
+
+    if (section === "messages") {
+      $scope.getMessages();
+    }
+  };
 
   $scope.setAuthentication = function (authentication) {
     localStorageService.set("authentication", authentication);
+
+    if (authentication) {
+      $scope.activateSection("messages");
+    }
   };
 
   $scope.getAuthentication = function () {
-    localStorageService.get("authentication");
+    return localStorageService.get("authentication");
+  };
+
+  $scope.getMessages = function () {
+    $scope.loading = true;
+    API.get("messages", {}, {}, function (data) {
+      $scope.messages = data.data;
+    });
   };
 
   constructor();
