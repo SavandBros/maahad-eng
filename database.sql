7acd9d24e14caf5b7ac3496a31ea05b7782905b9
Amir Mehdi Savand <amir@savandbros.com>
2017-08-19 22:28:25 +0400

    Product image can be 1000

--- database.sql
+++ database.sql
@@ -25,7 +25,7 @@ CREATE TABLE `products` (
  `id` int(11) NOT NULL AUTO_INCREMENT,
  `name` varchar(100) NOT NULL,
  `detail` text NOT NULL,
- `image` varchar(200) NOT NULL,
+ `image` varchar(1000) NOT NULL,
  `price` int(11) NOT NULL DEFAULT '0',
  `is_special` tinyint(1) NOT NULL DEFAULT '0',
  `ordering` int(11) NOT NULL,
