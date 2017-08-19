ed9aaeb61488b5cf8c9f1b17d8f9fa0e8a81f7f8
Amir Mehdi Savand <amir@savandbros.com>
2017-08-19 21:47:01 +0400

    Add ability to update products

--- api/products.php
+++ api/products.php
@@ -8,4 +8,25 @@ if ($method === "GET") {
 
 } else if ($method === "POST") {
 
+    if (!validate_authentication()) {
+        return;
+    }
+
+    if (require_params(["name"], $_POST)) {
+
+        $id = $db->quote($_POST["id"]);
+        $name = $db->quote($_POST["name"]);
+        $detail = $db->quote($_POST["detail"]);
+        $image = $db->quote($_POST["image"]);
+        $price = $db->quote($_POST["price"]);
+        $is_special = $db->quote($_POST["is_special"]);
+        $ordering = $db->quote($_POST["ordering"]);
+
+        $db->query(
+            "UPDATE products SET name=$name, detail=$detail, image=$image, ".
+            "price=$price, is_special=$is_special, ordering=$ordering WHERE id=$id"
+        );
+
+        response($db->affected(), 202);
+    }
 }
