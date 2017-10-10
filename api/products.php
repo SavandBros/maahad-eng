be07a8770dad7235e5f0c320be441e98b44a5de9
Amir Savand <amir@savandbros.com>
2017-10-11 02:26:21 +0400

    Remove price
    Fixes #23

--- api/products.php
+++ api/products.php
@@ -18,15 +18,11 @@ if ($method === "GET") {
         $name = $db->quote($_POST["name"]);
         $detail = $db->quote($_POST["detail"]);
         $image = $db->quote($_POST["image"]);
-        $price = $db->quote($_POST["price"]);
         $is_special = $db->quote($_POST["is_special"]);
         $ordering = $db->quote($_POST["ordering"]);
         $link = $db->quote($_POST["link"]);
 
-        $db->query(
-            "UPDATE products SET name=$name, detail=$detail, image=$image, ".
-            "price=$price, is_special=$is_special, ordering=$ordering, link=$link WHERE id=$id"
-        );
+        $db->query("UPDATE products SET name=$name, detail=$detail, image=$image, is_special=$is_special, ordering=$ordering, link=$link WHERE id=$id");
 
         response($db->affected(), 202);
     }
