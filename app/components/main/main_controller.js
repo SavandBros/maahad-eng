991a540d3f6349b8a2d8c5e2c803d7a38bbd8f8e
Amir Savand <amir@savandbros.com>
2018-06-17 07:44:41 +0400

    Add contact us panel

--- app/components/main/main_controller.js
+++ app/components/main/main_controller.js
@@ -1,6 +1,6 @@
 "use strict";
 
-app.controller("MainController", function (API, localStorageService, $scope, $rootScope, $state) {
+app.controller("MainController", function (API, localStorageService, toaster, $scope, $rootScope, $state) {
 
   function constructor() {
 
@@ -17,7 +17,53 @@ app.controller("MainController", function (API, localStorageService, $scope, $ro
      * @type {boolean}
      */
     $scope.isAdmin = localStorageService.get("authentication");
+
+    /**
+     * @type {boolean}
+     */
+    $scope.showContactPanel = false;
   }
+
+  /**
+   * @param {object} form
+   */
+  $scope.contact = function (form) {
+    console.log("Contacting from main...");
+    form.loading = true;
+    form.error = false;
+    form.success = false;
+
+    var payload = {
+      name: form.name || "",
+      number: form.number || "",
+      email: form.email,
+      message: form.message,
+      hidden: form.hidden,
+      // Required fields
+      product_name: "",
+      product_link: ""
+    };
+
+    API.post("messages", payload, {}, function () {
+      form.loading = false;
+      form.success = true;
+      toaster.success("Thank you", "We will contact you soon!");
+    }, function () {
+      form.loading = false;
+      toaster.error("Error", "Something went wrong, please try again.");
+    });
+  };
+
+  /**
+   * @event maahadEng.ContactController:contact
+   *
+   * @param {Event} event
+   * @param {object} form
+   */
+  $scope.$on("maahadEng.ContactController:contact", function (event, form) {
+    console.log("Recieving from main...");
+    $scope.contact(form);
+  });
 
   constructor();
 });
